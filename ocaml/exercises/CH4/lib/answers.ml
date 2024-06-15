5cb59abfc593a6a4c6842b50477affadf362a4e7
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2024-06-15 22:27:38 +0200

    CH4 almost finished

--- ocaml/exercises/CH4/lib/answers.ml
+++ ocaml/exercises/CH4/lib/answers.ml
@@ -66,4 +66,71 @@ let double_map f g = List.map (fun elt -> f (g elt))
 (* or *)
 let double_map' f g = List.map (f @@ g)
 
-(*  *)
+(* more list fun *)
+
+let strings_gt_3 = List.filter (fun x -> String.length x > 3)
+let add1_to_floats = List.map (fun x -> x +. 1.)
+
+let join_with sep strings =
+  List.fold_left
+    (fun acc elt -> if acc = "" then acc ^ elt else acc ^ sep ^ elt)
+    "" strings
+
+let x = List.sort
+
+(* association list keys *)
+
+let keys lst =
+  let rec keys_aux acc = function
+    | [] -> acc
+    | (key, _) :: t -> keys_aux (key :: acc) t
+  in
+  List.sort_uniq Stdlib.compare (keys_aux [] lst)
+
+(* from answers *)
+let keys' lst = lst |> List.rev_map fst |> List.sort_uniq Stdlib.compare
+
+(* valid matrix *)
+
+let is_valid_matrix = function
+  | [] -> false
+  | row :: rows ->
+    let row_len = List.length row in
+    List.for_all (fun row' -> List.length row' = row_len) rows
+
+(* row vector add *)
+
+let add_row_vectors = List.map2 ( + )
+
+(* matrix add *)
+
+let add_matrices = List.map2 (fun row1 row2 -> add_row_vectors row1 row2)
+
+(* from answers *)
+let add_matrices' = List.map2 add_row_vectors
+
+(* matrix multiply *)
+let rec get_col n = function
+  | [] -> []
+  | row :: rows -> List.nth row n :: get_col n rows
+
+let transpose matrix =
+  let rec transpose_aux i matrix' =
+    match matrix' with
+    | [] -> []
+    | row :: rows ->
+      if i >= List.length row then
+        []
+      else
+        get_col i matrix' :: transpose_aux (i + 1) rows
+  in
+  transpose_aux 0 matrix
+
+let multiply_row_vectors = List.map2 ( * )
+
+(* NOT DONE YET, NEED TO MAKE DOT FUNCTION *)
+let rec multiply_matrices matrix1 matrix2 =
+  match (matrix1, transpose matrix2) with
+  | row_m1 :: rows_m1, row_m2 :: rows_m2 ->
+    multiply_row_vectors row_m1 row_m2 :: multiply_matrices rows_m1 rows_m2
+  | _ -> failwith "arguments are not two matrices"
